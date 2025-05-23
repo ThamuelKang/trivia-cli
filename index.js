b3edb2c7232376fbdf80e18fc9ee47392ae20108
Samuel Kang <samuel.june.kang@gmail.com>
2025-05-23 15:13:47 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+import { program } from 'commander'
+import inquirer from "inquirer"
+import chalk from "chalk"
+import chalkAnimation from "chalk-animation"
+import { triviaQuestions } from "./src/question.js"
+import logUpdate from 'log-update';
+
+
+// helper function that pauses with specified ms parameters
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
+    });
+}
+
+// welcome
+async function welcome() {
+    const animation = chalkAnimation.rainbow('🦆 Duck Trivia 🧠\n');
+    await sleep(2000);
+    animation.stop();
+}
+
+async function askQuestion(questions) {
+    let score = 0;
+
+    for (let i = 0; i < questions.length; i++) {
+        const questionObject = questions[i];
+
+        // let secondsLeft = 5;
+        // const countdown = setInterval(() => {
+        //     logUpdate(`⏳ Time left: ${secondsLeft--}s`);
+        // }, 1000);
+
+        console.log('⏳ You have 10 seconds to answer!')
+
+        // used promise race to "race" two promises together, whichever one fufills first, is the select promise
+        const result = await Promise.race([
+            new Promise(resolve => {
+                setTimeout(() => resolve({ timedOut: true }), 10000);
+            }),
+            inquirer.prompt({
+                type: 'list',
+                name: 'userAnswer',
+                message: questionObject.question,
+                choices: questionObject.choices,
+            })
+        ]);
+
+        // clearInterval(countdown);
+
+        // Skips one instance of for loop and console logs 
+        if (result.timedOut) {
+            console.log(`\n⌛ Time's up! The correct answer was: ${questionObject.correctAnswer}\n`);
+            continue;
+        }
+
+        // Assigns userAnswers with selected answer
+        const userAnswer = result.userAnswer;
+
+        if (userAnswer === questionObject.correctAnswer) {
+            console.log("✅ Correct!\n");
+            score++;
+        } else {
+            console.log(`❌ Incorrect. The correct answer was: ${questionObject.correctAnswer}\n`);
+        }
+    }
+
+    // Final verdict
+
+    if (score >= 5) {
+        console.log(`\n Final score: ${score}! \n Wow! You r ducking smart!`)
+    } else {
+        console.log(`\n🎯 Final score: ${score}`);
+    }
+
+    process.exit();
+}
+
+
+// top level await is only available for es14 and above!
+await welcome();
+await askQuestion(triviaQuestions);
---
+++ src/question.js
@@ -0,0 +1,52 @@
+export const triviaQuestions = [
+    {
+        question: "1. What is the difference between a duck, a goose, and a swan?",
+        choices: [
+            "A. Ducks are larger than swans",
+            "B. Geese and swans are usually smaller than ducks",
+            "C. Ducks are usually smaller than geese and swans",
+            "D. Geese are usually larger than ducks and swans"
+        ],
+        correctAnswer: "C. Ducks are usually smaller than geese and swans"
+    },
+    {
+        question: "2. Why do ducks migrate?",
+        choices: [
+            "A. To become tiktok famous",
+            "B. To find a their dream job",
+            "C. To escape the cold and find a warmer climate",
+            "D. All of the above"
+        ],
+        correctAnswer: "C. To escape the cold and find a warmer climate"
+    },
+    {
+        question: "3. Which of these ducks are the smallest in size?",
+        choices: [
+            "A. Call duck",
+            "B. Mallards",
+            "C. Eider Ducks",
+            "D. You 🫵"
+        ],
+        correctAnswer: "A. Call duck"
+    },
+    {
+        question: "4. What part of the duck makes its feather waterproof?",
+        choices: [
+            "A. Webbed feet",
+            "B. Beak",
+            "C. Tail feathers",
+            "D. Urpoygial gland"
+        ],
+        correctAnswer: "D. Urpoygial gland"
+    },
+    {
+        question: "5. Which duck species is known for its bright green head?",
+        choices: [
+            "A. Tiktok Duck",
+            "B. Mallards",
+            "C. Scaup",
+            "D. Merganser"
+        ],
+        correctAnswer: "B. Mallards"
+    }
+]
